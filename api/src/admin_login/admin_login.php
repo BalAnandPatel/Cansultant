ae69c40c26eeea4bf8ba718cd69b64dfd1ea5d59
SHIKHGLINTEL <shikha.sharma@glintel.com>
2022-10-19 17:45:49 +0530

    updated

--- api/src/admin_login/admin_login.php
+++ api/src/admin_login/admin_login.php
@@ -1,72 +1,69 @@
 <?php
-// required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  
-// database connection will be here
-
-// include database and object files
 include_once '../../config/database.php';
-include_once '../../objects/admin_login.php';
-  
-// instantiate database and product object
+include_once '../../objects/registration.php';
+require '../../../common/php-jwt/src/JWT.php';
+require '../../../common/php-jwt/src/ExpiredException.php';
+require '../../../common/php-jwt/src/SignatureInvalidException.php';
+require '../../../common/php-jwt/src/BeforeValidException.php';
+require '../../../constant.php';
+use \Firebase\JWT\JWT;
+
+ $issuedat_claim = time(); // issued at
+ $notbefore_claim = $issuedat_claim ; //not before in seconds
+ $expire_claim = $issuedat_claim + 30; // expire time in seconds
+
+$data = json_decode(file_get_contents("php://input"));
+//var_dump($data);
 $database = new Database();
 $db = $database->getConnection();
-  
-// initialize object
-$admin = new admin($db);
-  
-$data = json_decode(file_get_contents("php://input"));
-// read products will be here
+$login = new admin($db);
+$login->email = $data->email;
+$login->password = $data->password;
+//$login->updated_on=date('Y-m-d H:i:s');
 
-$admin->email=$data->email;
-$admin->password=$data->password;
-// query products
-$stmt = $admin->login();
+$stmt = $login->adm_login();
 $num = $stmt->rowCount();
-  
-// check if more than 0 record found
-if($num>0){
-  
-    // products array
-    $admins_arr=array();
-    $admins_arr["records"]=array();
-
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-     
+if($num>0){       
+        
+if ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+       // var_dump($row);
         extract($row);
-  
-        $admin_item=array(
-
-            "id" => $id,
-            "email"=>$email,
-            "password"=>$password,
-           
-        );
-  
-        array_push($admins_arr["records"], $admin_item);
-    }
-  
-    // show products data in json format
-    echo json_encode($admins_arr);
-
-     // set response code - 200 OK
-     http_response_code(200);
+	$token = array(
+            "iss" => $ISSUER_CLAIM,
+            "aud" => $AUDIENCE_CLAIM,
+            "iat" => $issuedat_claim,
+            "nbf" => $notbefore_claim,
+            "exp" => $expire_claim,
+            "data" => array(
+                "message" => $LOGIN_SUCCESS_MSG,
+                "email" => $email,
+                "full_name" =>$full_name,
+                "password" =>$password,
+                "id"=>$id,
+                "created_on"=>$created_on,
+                
+               
+        ));
+	//var_dump($token);
+   $jwt = JWT::encode($token, $SECRET_KEY);     
+   
+        echo json_encode(
+            array(
+                "access_token" => $jwt
+            ));
+        }
+        http_response_code(200);
 }
-  
-// no products found will be here
+
 else{
-  
-    // set response code - 404 Not found
-    http_response_code(404);
-  
-    // tell the user no products found
-    echo json_encode(
-        array("message" => "No user found.")
-    );
+
+        http_response_code(401);
+        echo json_encode(array("message" => $LOGIN_FAILED_MSG));
 }
+
 ?>
